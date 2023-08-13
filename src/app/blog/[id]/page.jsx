d1b8b46d973e49475d2943e9e7052e989f7fd1cd
Shajib-Dv <mdsojeeb242@gmail.com>
2023-08-13 23:48:36 +0600

    blog added

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,7 +1,26 @@
 /** @format */
 
-const blogPost = () => {
-  return <div>blogPost</div>;
+import Image from "next/image";
+import img from "/public/images/lawyer-3.jpg";
+
+const blogPost = ({ params }) => {
+  return (
+    <div className="md:p-20 p-4">
+      <div className="flex md:flex-row flex-col-reverse items-center gap-10">
+        <div className="flex-1 p-4 space-y-8">
+          <h2 className="text-4xl font-bold">Hot news from {params?.id}</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta nemo
+            cumque earum libero cum laudantium numquam repellendus deserunt vero
+            magni.
+          </p>
+        </div>
+        <div className="flex-1">
+          <Image src={img} alt="img" className="rounded-lg object-cover" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default blogPost;
